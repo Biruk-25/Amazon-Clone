f22d58c978721ba9df965b58e5401e8bbe912fdd
Biruk <birukelias2@gmail.com>
2025-05-20 09:15:50 -0500

    Fix base URL issues

--- vite.config.js
+++ vite.config.js
@@ -1,16 +1,16 @@
-// import { defineConfig } from 'vite'
-// import react from '@vitejs/plugin-react'
-
-// export default defineConfig({
-//   base: "/Amazon-Clone/",
-//   plugins: [react()]
-// })
-
-// vite.config.js
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
 export default defineConfig({
-  plugins: [react()],
-  base: process.env.NODE_ENV === 'production' ? '/Amazon-Clone/' : '/', // ✅ dynamic base
+  base: "/Amazon-Clone/",
+  plugins: [react()]
 })
+
+// vite.config.js
+// import { defineConfig } from 'vite'
+// import react from '@vitejs/plugin-react'
+
+// export default defineConfig({
+//   plugins: [react()],
+//   base: process.env.NODE_ENV === 'production' ? '/Amazon-Clone/' : '/', // ✅ dynamic base
+// })
