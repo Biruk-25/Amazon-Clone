d9ab6f30f4a0872ad682e9f775663484618f8bf0
Biruk <birukelias2@gmail.com>
2025-05-21 03:45:52 -0500

    Update carousel fade effect

--- src/Components/Carousel/img/data.js
+++ src/Components/Carousel/img/data.js
@@ -1,12 +1,7 @@
-import img1 from './imgA.jpg';
-import img2 from './imgB.jpg';
-import img4 from './imgD.jpg';
-import img6 from './imgC.jpg';
-import img7 from './10002 (1).jpg';
-import img8 from './10005 (1).jpg';
-import img11 from './10003 (1).jpg';
-import img12 from './10004 (1).jpg';
-import img16 from './imgE.jpg';
+import img1 from './10001.jpg'
+import img2 from './10002.jpg'
+import img3 from './10003.jpg'
+import img4 from './10004.jpg'
+import img5 from './10005.jpg'
 
-
-export const img = [img1,img2,img4,img6,img7,img8,img11,img12,img16];
+export const img = [img1,img2,img3,img4,img5]
